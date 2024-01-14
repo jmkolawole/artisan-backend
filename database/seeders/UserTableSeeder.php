8c2a0efb39977c96b8b7875cbf4e53f83574d9c9
jmkolawole <jmkolawole@gmail.com>
2024-01-14 22:27:18 +0100

    add to seeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -21,15 +21,34 @@ class UserTableSeeder extends Seeder
     $kolawole->username = 'korlahwarleh';
     $kolawole->email = 'jmkolawole@gmail.com';
     $kolawole->password = bcrypt('Pa$$w0rd');
+    $kolawole->is_admin = 1;
     $kolawole->save();
 
 
-    $anesi = new User();
-    $anesi->firstname = 'Anesi';
-    $anesi->lastname = 'Igebu';
-    $anesi->username = 'gabriella';
-    $anesi->email = 'anesi@gmail.com';
-    $anesi->password = bcrypt('Pa$$w0rd');
-    $anesi->save();
+    $uzo = new User();
+    $uzo->firstname = 'Uzo';
+    $uzo->lastname = 'Uzo';
+    $uzo->username = 'uzo';
+    $uzo->email = 'uzo12@gmail.com';
+    $uzo->password = bcrypt('Pa$$w0rd');
+    $uzo->is_admin = 1;
+    $uzo->save();
+
+    $carpenter = new User();
+    $carpenter->firstname = 'Carpenter';
+    $carpenter->lastname = 'Carpenter';
+    $carpenter->username = 'carpenter';
+    $carpenter->email = 'carpenter@gmail.com';
+    $carpenter->password = bcrypt('Pa$$w0rd');
+    $carpenter->save();
+
+    $plumber = new User();
+    $plumber->firstname = 'Plumber';
+    $plumber->lastname = 'Plumber';
+    $plumber->username = 'Plumber';
+    $plumber->email = 'plumber@gmail.com';
+    $plumber->password = bcrypt('Pa$$w0rd');
+    $plumber->save();
+    
   }
 }
